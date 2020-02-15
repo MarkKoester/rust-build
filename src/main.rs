b5aa65fb9c82c326e482f1e2dfcaa9bf9f249ed0
Mark Koester <mark.koester1@gmail.com>
2020-02-15 15:34:19 -0500

    Compile test file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::process::Command;
+use std::process::Output;
+use std::str;
+use std::fs;
+
 fn main() {
+    let result = compile("main.cpp");
+    let stderr = str::from_utf8(&result.stderr);
+    println!("{}", result.status);
+    println!("{:#?}", stderr);
     println!("Hello, world!");
+}
+
+fn compile(path: &str) -> Output {
+    let working_directory = fs::canonicalize("test/").unwrap();
+
+    let mut command = Command::new("gcc");
+    command.arg(&path).arg("-o").arg("out/a.out").current_dir(working_directory);
+    println!("{:?}", command);
+    let result = command.output().expect("failed to compile");
+    result
 }
